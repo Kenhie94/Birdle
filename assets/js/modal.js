80e148d1cc9fd0651a84a30dd142ce54dbb3f177
Magsmags600 <margaretjenkins@gmail.com>
2024-09-13 14:10:39 -0700

    update modals

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,46 +1,44 @@
+// Get modal elements - How to Play
+const howToPlayEl = document.getElementById("howToPlayEl");
+const howToPlayBtn = document.getElementById("howToPlayBtn");
+const howToPlayClose = document.getElementById("howToPlayClose"); // Unique ID for close button
 
-      // Get modal elements - ABOUT US
-      var modal1 = document.getElementById("myModal1");
-      var btn = document.getElementById("openModalBtn");
-      var span = document.getElementsByClassName("close")[0];
+// Open the modal
+howToPlayBtn.onclick = function() {
+    howToPlayEl.style.display = "block";
+}
 
-      // Open the modal
-      btn.onclick = function() {
-          modal1.style.display = "block";
-      }
+// Close the modal
+howToPlayClose.onclick = function() {
+    howToPlayEl.style.display = "none";
+}
 
-      // Close the modal
-      span.onclick = function() {
-          modal1.style.display = "none";
-      }
+// Close modal when user clicks outside of it
+window.onclick = function(event) {
+    if (event.target == howToPlayEl) {
+        howToPlayEl.style.display = "none";
+    }
+}
 
-      // Close modal when user clicks outside of it
-      window.onclick = function(event) {
-          if (event.target == modal1) {
-              modal1.style.display = "none";
-          }
-      }
- 
-      
-      // Get modal elements - How to Play
-      var modal2 = document.getElementById("myModal2");
-      var btn = document.getElementById("openModalBtn2");
-      var span = document.getElementsByClassName("close2")[1];
+// Get modal elements - Stats
+const statsEl = document.getElementById("statsEl");
+const statsBtn = document.getElementById("statsBtn");
+const statsClose = document.getElementById("statsClose"); // Unique ID for close button
 
-      // Open the modal
-      btn.onclick = function() {
-          modal2.style.display = "block";
-      }
+// Open the modal
+statsBtn.onclick = function() {
+    statsEl.style.display = "block";
+}
 
-      // Close the modal
-      span.onclick = function() {
-          modal2.style.display = "none";
-      }
+// Close the modal
+statsClose.onclick = function() {
+    statsEl.style.display = "none";
+}
 
-      // Close modal when user clicks outside of it
-      window.onclick = function(event) {
-          if (event.target == modal2) {
-              modal2.style.display = "none";
-          }
-      }
- 
+// Close modal when user clicks outside of it
+window.onclick = function(event) {
+    if (event.target == statsEl) {
+        statsEl.style.display = "none";
+    }
+}
+
